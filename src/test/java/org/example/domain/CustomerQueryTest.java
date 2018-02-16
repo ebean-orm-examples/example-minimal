a535f8c85387c7ba811611a72d541a83e0550e63
Rob Bygrave <robin.bygrave@gmail.com>
2018-02-16 14:09:22 +1300

    Add query bean support ...
    - Make sure we have a ebean.mf ... with the querybean-packages specified
    - Modify the pom adding the dependency for ebean-querybean and querybean-generator (provided)

--- src/test/java/org/example/domain/CustomerQueryTest.java
+++ src/test/java/org/example/domain/CustomerQueryTest.java
@@ -1,6 +1,7 @@
 package org.example.domain;
 
 import io.ebean.Ebean;
+import org.example.domain.query.QCustomer;
 import org.testng.annotations.Test;
 
 public class CustomerQueryTest {
@@ -10,5 +11,9 @@ public class CustomerQueryTest {
 
     Ebean.find(Customer.class)
         .findList();
+
+    new QCustomer()
+      .id.greaterOrEqualTo(1L)
+      .findList();
   }
 }
