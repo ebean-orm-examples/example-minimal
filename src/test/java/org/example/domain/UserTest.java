f12144157b50b0ba4a4239b2b57da94aae5c23a5
Miltiadis Orfanos <miltiadis02@gmail.com>
2022-05-10 01:16:28 -0400

    Updates test

--- src/test/java/org/example/domain/UserTest.java
+++ src/test/java/org/example/domain/UserTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -35,18 +36,17 @@ class UserTest {
       server.save(user);
     }
 
+    // update roles
+    {
     User fetchedUser = server.find(User.class).where().eq("name", "rob").findOne();
     Objects.requireNonNull(fetchedUser);
-
-    {
       Role role = new Role();
       role.setName("superadmin");
       List<Role> superadmin = List.of(role);
 
-      List<Role> updatedRoles = Stream.concat(
-        fetchedUser.getRoles().stream().filter(r -> "admin".equals(r.getName())),
-        superadmin.stream()
-      ).collect(Collectors.toList());
+      List<Role> updatedRoles = new ArrayList<>();
+      updatedRoles.addAll(fetchedUser.getRoles().stream().filter(r -> "admin".equals(r.getName())).collect(Collectors.toList()));
+      updatedRoles.addAll(superadmin);
 
       fetchedUser.setRoles(updatedRoles);
       server.save(fetchedUser);
