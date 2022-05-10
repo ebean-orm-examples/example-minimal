beed2080cb5b013b6fee8aad0943355717fea699
Miltiadis Orfanos <miltiadis02@gmail.com>
2022-05-10 01:08:50 -0400

    Reverts query test

--- src/test/java/org/example/domain/CustomerQueryTest.java
+++ src/test/java/org/example/domain/CustomerQueryTest.java
@@ -1,7 +1,7 @@
 package org.example.domain;
 
 import io.ebean.DB;
-//import org.example.domain.query.QCustomer;
+import org.example.domain.query.QCustomer;
 import org.junit.jupiter.api.Test;
 
 public class CustomerQueryTest {
@@ -12,8 +12,8 @@ public class CustomerQueryTest {
     DB.find(Customer.class)
         .findList();
 
-//    new QCustomer()
-//      .id.greaterOrEqualTo(1L)
-//      .findList();
+    new QCustomer()
+      .id.greaterOrEqualTo(1L)
+      .findList();
   }
 }
